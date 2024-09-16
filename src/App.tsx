5e528361bc890a9cdf47f15fddac598372bf7089
Aymen Hammami <hello@aymen-hammami.com>
2024-09-16 12:26:29 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ChakraProvider } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <ChakraProvider>
+      <h1>Game Hub</h1>
+    </ChakraProvider>
+  )
+}
+
+export default App
