1df67e8105ead019a186ea7905977e08a8a085e0
Aymen Hammami <hello@aymen-hammami.com>
2024-09-17 22:41:20 +0100

    Filter games by platform

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import GenreList from "./components/GenreList.tsx";
 import {useState} from "react";
 import {Genre} from "./hooks/useGenre.ts";
 import PlatformSelector from "./components/PlatformSelector.tsx";
+import {Platform} from "./hooks/usePlatforms.ts";
 
 function App() {
     const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+    const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
     return (
         <>
             <Grid
@@ -29,8 +31,9 @@ function App() {
                     </GridItem>
                 </Show>
                 <GridItem pl='2' area={'main'}>
-                    <PlatformSelector/>
-                    <GameGrid selectedGenre={selectedGenre}/>
+                    <PlatformSelector selectedPlatform={selectedPlatform}
+                                      onSelectPlatform={(platform) => setSelectedPlatform(platform)}/>
+                    <GameGrid selectedGenre={selectedGenre} selectedPlatform={selectedPlatform}/>
                 </GridItem>
             </Grid>
         </>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,13 +4,15 @@ import GameCard from "./GameCard.tsx";
 import GameCardSkeleton from "./GameCardSkeleton.tsx";
 import GameCardContainer from "./GameCardContainer.tsx";
 import {Genre} from "../hooks/useGenre.ts";
+import {Platform} from "../hooks/usePlatforms.ts";
 
 interface GameGridProps {
     selectedGenre: Genre | null;
+    selectedPlatform: Platform | null;
 }
 
-const GameGrid = ({selectedGenre}: GameGridProps) => {
-    const {data, error, isLoading} = useGames(selectedGenre);
+const GameGrid = ({selectedGenre, selectedPlatform}: GameGridProps) => {
+    const {data, error, isLoading} = useGames(selectedGenre, selectedPlatform);
     const skeletons = [1, 2, 3, 4, 5, 6];
     return (
         <>
