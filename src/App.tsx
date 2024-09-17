c050b38851fceabdc205a7727ec20e74120ee6b6
Aymen Hammami <hello@aymen-hammami.com>
2024-09-17 10:11:17 +0100

    Build the theme color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,11 @@ function App() {
                     <NavBar/>
                 </GridItem>
                 <Show above="lg">
-                    <GridItem pl='2' bg='pink.300' area={'aside'}>
+                    <GridItem pl='2' area={'aside'}>
                         Aside
                     </GridItem>
                 </Show>
-                <GridItem pl='2' bg='green.300' area={'main'}>
+                <GridItem pl='2' area={'main'}>
                     Main
                 </GridItem>
             </Grid>
