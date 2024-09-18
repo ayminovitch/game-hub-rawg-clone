7b32791e170418748841bf6cafe6d772e3cf0fa4
Aymen Hammami <hello@aymen-hammami.com>
2024-09-18 10:57:22 +0100

    Implement searching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,15 @@ import logo from '../assets/logo.webp';
 import ColorModeSwitch from "./ColorModeSwitch.tsx";
 import SearchInput from "./SearchInput.tsx";
 
-const NavBar = () => {
+interface Props {
+    onSearch: (searchQuery: string) => void;
+}
+
+const NavBar = ({onSearch}: Props) => {
     return (
         <HStack padding='10px'>
             <Image src={logo} boxSize='60px'/>
-            <SearchInput/>
+            <SearchInput onSearch={onSearch}/>
             <ColorModeSwitch/>
         </HStack>
     );
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client'
 import {ChakraProvider, ColorModeScript} from '@chakra-ui/react'
 import App from './App.tsx'
 import theme from './theme';
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
