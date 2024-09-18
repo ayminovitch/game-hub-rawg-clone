c580bc99ffa738f89e10db241bafabfcd86f9062
Aymen Hammami <hello@aymen-hammami.com>
2024-09-18 11:22:21 +0100

    Clean up the games cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,11 +13,11 @@ const GameCard = ({game}: GameCardProps) => {
         <Card>
             <Image src={getCroppedImageUrl(game.background_image)}></Image>
             <CardBody>
-                <Heading fontSize='2xl'>{game.name}</Heading>
-                <HStack justifyContent='space-between'>
+                <HStack justifyContent='space-between' marginBottom={3}>
                     <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
                     <CriticScore score={game.metacritic}/>
                 </HStack>
+                <Heading fontSize='2xl'>{game.name}</Heading>
             </CardBody>
         </Card>
     );
