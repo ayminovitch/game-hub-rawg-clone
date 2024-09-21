761b34c27e2e692f2f4d19ddbdb5be28c76f0642
Aymen Hammami <hello@aymen-hammami.com>
2024-09-21 10:05:25 +0100

    Remove- react query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 import {ChakraProvider, ColorModeScript} from '@chakra-ui/react'
 import App from './App.tsx'
 import theme from './theme';
@@ -15,7 +14,6 @@ createRoot(document.getElementById('root')!).render(
             <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
             <QueryClientProvider client={queryClient}>
                 <App/>
-                <ReactQueryDevtools/>
             </QueryClientProvider>
         </ChakraProvider>
     </StrictMode>,
