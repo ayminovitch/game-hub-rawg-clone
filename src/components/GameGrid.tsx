a15072f4c79ef66e3ded044c87d47b8492c4c788
Aymen Hammami <hello@aymen-hammami.com>
2024-09-21 11:08:32 +0100

    Implement infinit scroll

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,25 +1,33 @@
 import React from "react";
-import {Box, Button, SimpleGrid, Text} from "@chakra-ui/react";
+import {SimpleGrid, Spinner, Text} from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard.tsx";
 import GameCardSkeleton from "./GameCardSkeleton.tsx";
 import GameCardContainer from "./GameCardContainer.tsx";
 import {GameQuery} from "../App.tsx";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface GameGridProps {
     gameQuery: GameQuery;
 }
 
 const GameGrid = ({gameQuery}: GameGridProps) => {
-    const {data, error, isLoading, fetchNextPage, isFetchingNextPage, hasNextPage} = useGames(gameQuery);
+    const {data, error, isLoading, fetchNextPage, hasNextPage} = useGames(gameQuery);
     const skeletons = [1, 2, 3, 4, 5, 6];
 
     if (error) return <Text>{error.message}</Text>
+
+    const fetchedGamesCount = data?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
     return (
-        <Box padding='10px'>
+        <InfiniteScroll
+            dataLength={fetchedGamesCount}
+            hasMore={!!hasNextPage}
+            next={() => fetchNextPage()}
+            loader={<Spinner/>}
+        >
             <SimpleGrid columns={{
                 sm: 1, md: 2, lg: 3, xl: 4,
-            }} spacing={6}>
+            }} spacing={6} padding='10px'>
                 {isLoading && skeletons.map(skeleton => (
                     <GameCardContainer key={skeleton}><GameCardSkeleton/></GameCardContainer>))}
 
@@ -33,10 +41,7 @@ const GameGrid = ({gameQuery}: GameGridProps) => {
                     </React.Fragment>))
                 }
             </SimpleGrid>
-            {hasNextPage && <Button disabled={isFetchingNextPage}
-                                    onClick={() => fetchNextPage()}
-                                    marginY={5}>{isFetchingNextPage ? 'Loading...' : 'Load more'}</Button>}
-        </Box>
+        </InfiniteScroll>
     );
 };
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -30,6 +30,7 @@ const useGames = (gameQuery: GameQuery) => useInfiniteQuery<FetchResponse<Game>,
         return lastPage.next ? allPages.length + 1 : undefined;
     },
     keepPreviousData: true,
+    staleTime: 24 * 60 * 60 * 1000, //24h
 })
 
 export default useGames;
