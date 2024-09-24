2324a13bf869322a80690f31baab928aeb1d04f2
Aymen Hammami <hello@aymen-hammami.com>
2024-09-25 00:13:43 +0100

    Improve game detail layout

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,6 @@
 import {useParams} from "react-router-dom";
 import useGame from "../hooks/useGame.ts";
-import {Heading, Spinner} from "@chakra-ui/react";
+import {GridItem, Heading, SimpleGrid, Spinner} from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText.tsx";
 import GameAttributes from "../components/GameAttributes.tsx";
 import GameTrailer from "../components/GameTrailer.tsx";
@@ -16,11 +16,20 @@ const GameDetailPage = () => {
 
     return (
         <>
-            <Heading>{game.name}</Heading>
-            <ExpandableText>{game.description_raw}</ExpandableText>
-            <GameAttributes game={game}/>
-            <GameTrailer gameId={game.id}/>
-            <GameScreenshots gameId={game.id}/>
+
+            <SimpleGrid columns={{
+                base: 1, md: 2
+            }} spacing={5}>
+                <GridItem>
+                    <Heading>{game.name}</Heading>
+                    <ExpandableText>{game.description_raw}</ExpandableText>
+                    <GameAttributes game={game}/>
+                </GridItem>
+                <GridItem>
+                    <GameTrailer gameId={game.id}/>
+                    <GameScreenshots gameId={game.id}/>
+                </GridItem>
+            </SimpleGrid>
         </>
     );
 };
